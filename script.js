655260ebea6cf5874f2e52f20186ac0198d39040
helloraya <soraya.ismah12@guru.smp.belajar.id>
2026-06-19 14:25:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,17 +33,17 @@ const DATA_BRACKET = {
     "g8_r1": "8A", "g8_r2": "8C", "g8_r3": "8B",
     "m1_p1": "7B", "m1_p2": "8C",
     "m2_p1": "8A", "m2_p2": "7C",
-    "m3_p1": "WINNER M1", "m3_p2": "WINNER M2",
+    "m3_p1": "8C", "m3_p2": "WINNER M2",
     "m4_p1": "-", "m4_p2": "-", "champ": "[ TBD ]", "third": "-"
   },
   "Kasti": {
-    "g7_match": "A vs B | B vs C | C vs A",
-    "g7_r1": "-", "g7_r2": "-", "g7_r3": "-",
-    "g8_match": "A vs B | B vs C | C vs A",
-    "g8_r1": "-", "g8_r2": "-", "g8_r3": "-",
-    "m1_p1": "RANK 1 G7", "m1_p2": "RANK 2 G8",
-    "m2_p1": "RANK 1 G8", "m2_p2": "RANK 2 G7",
-    "m3_p1": "WINNER M1", "m3_p2": "WINNER M2",
+    "g7_match": "7B vs 7C | 7C vs 7A | 7A vs 7B",
+    "g7_r1": "7C", "g7_r2": "7B", "g7_r3": "7A",
+    "g8_match": "8B vs 8A | 8C vs 8B | 8A vs 8C",
+    "g8_r1": "8B", "g8_r2": "8A", "g8_r3": "8C",
+    "m1_p1": "7C", "m1_p2": "8A",
+    "m2_p1": "8B", "m2_p2": "7B",
+    "m3_p1": "8A", "m3_p2": "WINNER M2",
     "m4_p1": "-", "m4_p2": "-", "champ": "[ TBD ]", "third": "-"
   }
 };
